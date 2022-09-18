9151bc4ec8b6c93b57479ee79513e370feb9ac38
Vishal Kella <vishal.kella@gmail.com>
2022-09-18 10:13:32 -0400

    added about

--- script.py
+++ script.py
@@ -4,30 +4,22 @@ from pathlib import Path
 from urllib.parse import parse_qsl, urljoin
 import unidecode
 
-directory_path = Path('/Users/vishalk/personal-repo/urdu-poetry-circle/docs/poets')
-
+directory_path = Path(
+    '/Users/vishalk/personal-repo/urdu-poetry-circle/docs/poets')
 
 file_path = '/Users/vishalk/Downloads/Catalog-Grid view.csv'
 
 
-
 def read_file(file_path: str):
-    # poet_count = {}
     rows = []
     with open(file_path, mode='r', encoding='utf-8-sig') as csv_file:
         csv_reader = csv.DictReader(csv_file)
         for line in csv_reader:
             rows.append(line)
-            # poet = line['Poet']
-            # if poet in poet_count:
-            #     poet_count[poet] += 1
-            # else:
-            #     poet_count[poet] = 1
-    # poet_count = dict(sorted(poet_count.items(), key=lambda item: item[1], reverse=True))
     return rows
 
 
-def generate_yt_embedded(link:str) -> str:
+def generate_yt_embedded(link: str) -> str:
     if "youtube" in link:
         embedded = link.replace("watch?v=", "embed/")
         return f'''<iframe width="560" height="315" src="{embedded}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'''
@@ -35,7 +27,7 @@ def generate_yt_embedded(link:str) -> str:
         return link
 
 
-def generate_music_section(item:dict, file):
+def generate_music_section(item: dict, file):
     file.write('### Renditions & Recitations\n\n')
     music_links = item['Music'].split('- ')[1:]
     for link in music_links:
@@ -58,9 +50,9 @@ def generate_text_section(item, file):
         file.write(rekhta)
     else:
         file.write('')
-        
-        
-def generate_others(item:dict, file):
+
+
+def generate_others(item: dict, file):
     file.write('### Others\n\n')
     other_links = item['Others'].split('- ')[1:]
     for link in other_links:
@@ -74,8 +66,7 @@ def generate_others(item:dict, file):
             file.write(f'#### {s[0].strip()}\n\n')
             l = s[1].strip()
             file.write(f'{l}\n\n')
-    
-    
+
 
 def generate_md(file_path: str):
     rows = read_file(file_path)
@@ -98,8 +89,6 @@ def generate_md(file_path: str):
             file.writelines(boiler_text)
             generate_text_section(item, file)
             if item['Music']:
-                # pdb.set_trace()
-                # print(item['Music'])
                 generate_music_section(item, file)
             if item['Others']:
                 generate_others(item, file)
